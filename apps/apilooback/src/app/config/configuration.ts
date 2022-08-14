ae501d7124a98ebd1b02801637375dc38ad51287
Sam <samuel.s.mamane@gmail.com>
2022-08-15 00:39:44 +0200

    Hope it works

--- apps/apilooback/src/app/config/configuration.ts
+++ apps/apilooback/src/app/config/configuration.ts
@@ -1,10 +1,10 @@
 export default (entities) => ({
-  type: process.env.DB_TYPE,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  type: process.env.DB_TYPE || 'mysql',
+  host: process.env.DB_HOST || '34.78.18.91',
+  port: process.env.DB_PORT || 3306,
+  username: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'apiloo',
   synchronize: true,
   entities,
 });
